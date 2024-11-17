95a969c658ecc29598c579d73d4d344688508529
abdalrhman02 <abdalrhmandev10@gmail.com>
2024-11-17 13:07:48 +0300

    Build Order Management Page (80%)

--- src/Pages/Admin-Pages/AdminOrdersManagement.js
+++ src/Pages/Admin-Pages/AdminOrdersManagement.js
@@ -1,12 +1,27 @@
 import React, { useRef, useState } from "react";
 
 // Components
-    import AdminHeader from '../../Components/Admin-Pages-Comps/AdminHeader'
-    import AdminFooter from '../../Components/Admin-Pages-Comps/AdminFooter'
+import AdminHeader from '../../Components/Admin-Pages-Comps/AdminHeader'
+import AdminFooter from '../../Components/Admin-Pages-Comps/AdminFooter'
 import AdminSideBar from '../../Components/Admin-Pages-Comps/AdminSideBar';
+import ProductCard from '../../Components/ProductCard';
 
 function AdminOrdersManagement() {
 
+    const orders = [
+        {id:425912 , customerName:'احمد' , town:'عكا' , phone:'0909090909' , altPhone:'0808080808' , date:'11/11/2024' , price:170 , status:'done' , note:"NoteHere", products: ["Product-1" , "Product-2"] },
+        {id:612422 , customerName:'سامي' , town:'ناصرة' , phone:'0909090909' , altPhone:'0808080808' , date:'11/11/2024' , price:130 , status:'waiting' , note:"NoteHere" , products: ["Product-1" , "Product-2"] },
+        {id:927557 , customerName:'سمر' , town:'حيفا' , phone:'0909090909' , altPhone:'0808080808' , date:'11/11/2024' , price:60 , status:'waiting' , note:"NoteHere" , products: ["Product-1" , "Product-2"] },
+        {id:206278 , customerName:'صلاح' , town:'القدس' , phone:'0909090909' , altPhone:'0808080808' , date:'11/11/2024' , price:240 , status:'done' , note:"NoteHere" , products: ["Product-1" , "Product-2"] },
+        {id:290512 , customerName:'محمد' , town:'القدس' , phone:'0909090909' , altPhone:'0808080808' , date:'11/11/2024' , price:350 , status:'canceled' , note:"NoteHere" , products: ["Product-1" , "Product-2"] },
+        {id:261062 , customerName:'مريم' , town:'طمرة' , phone:'0909090909' , altPhone:'0808080808' , date:'11/11/2024' , price:150 , status:'delivery' , note:"NoteHere" , products: ["Product-1" , "Product-2"] }
+    ]
+
+    const [expandOrder, setExpandOrder] = useState(null);
+
+    const toggleOrderDetails = (orderId) => {
+        setExpandOrder(expandOrder === orderId ? null : orderId);
+    };
 
     return (
         <div className="admin-orders-management">
@@ -42,6 +57,65 @@ function AdminOrdersManagement() {
                                 <p>عدد الطلبيات: 8</p>
                             </div>
                         </div>
+
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th>رقم الطلبية</th>
+                                    <th>اسم الزبون</th>
+                                    <th>البلد</th>
+                                    <th>رقم الهاتف</th>
+                                    <th>تاريخ الطلبية</th>
+                                    <th>المبلغ</th>
+                                    <th>حالة الطلبية</th>
+                                </tr>
+                            </thead>
+
+                            <tbody>
+                                {orders.map((order) => (
+                                    <React.Fragment key={order.id}>
+                                        <tr onClick={() => toggleOrderDetails(order.id)}>
+                                            <td>{order.id}</td>
+                                            <td>{order.customerName}</td>
+                                            <td>{order.town}</td>
+                                            <td>{order.phone}</td>
+                                            <td>{order.date}</td>
+                                            <td>₪{order.price}</td>
+                                            <td>
+                                                <div className={`status ${order.status}`}>
+                                                    <p>
+                                                        {order.status === "waiting" && "انتظار"}
+                                                        {order.status === "done" && "تم"}
+                                                        {order.status === "canceled" && "الغاء"}
+                                                        {order.status === "delivery" && "توصيل"}
+                                                    </p>
+                                                </div>
+                                            </td>
+                                        </tr>
+                                        
+                                        {expandOrder === order.id && (
+                                            <tr>
+                                                <td colSpan="7" style={{backgroundColor:"#fff"}}>
+                                                    <div className="order-details">
+                                                        <h4>تفاصيل الطلبية:</h4>
+                                                        <ul>
+                                                            {order.products.map((product, index) => (
+                                                                <li key={index}>{product}</li>
+                                                            ))}
+                                                        </ul>
+
+                                                        <br />
+
+                                                        <h4>ملاحظة الزبون:</h4>
+                                                        <p>{order.note}</p>
+                                                    </div>
+                                                </td>
+                                            </tr>
+                                        )}
+                                    </React.Fragment>
+                                ))}
+                            </tbody>
+                        </table>
                     </div>
                 </div>
             </div>
